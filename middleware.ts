b55cc0da70b58d1b43c95d125691ccfa6bf44d27
Arjun Iyer <arjun58634@gmail.com>
2025-08-07 04:36:42 -0400

    Simplify WorkOS middleware to absolute basics

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,6 @@ import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
 
 export default authkitMiddleware();
 
-// Only apply middleware to specific paths that need WorkOS
 export const config = {
-  matcher: [
-    '/api/debug-auth',
-    '/((?!_next/static|_next/image|favicon.ico|public|api/webhooks).*)'
-  ]
+  matcher: ['/']
 }
